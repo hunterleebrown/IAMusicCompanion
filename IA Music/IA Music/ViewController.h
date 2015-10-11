dc525bc7077db1567bf94b88bd8c00150c747957
brownh <hunter.brown@cbsinteractive.com>
2015-10-10 18:13:38 -0700

    Init Xcode project

---
+++ IA Music/IA Music/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IA Music
+//
+//  Created by Hunter on 10/10/15.
+//  Copyright © 2015 Hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
